9fe5554037f5025bdd344846e25d6eb65410425a
Igor <18249562+djestat@users.noreply.github.com>
2022-03-04 23:38:11 +0300

    Lesson 1
    updates

---
+++ GB-SwiftUI/GB-SwiftUI/GB_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GB_SwiftUIApp.swift
+//  GB-SwiftUI
+//
+//  Created by Igor on 04.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct GB_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
