7fcda6220a26237bba5187404675b3f2edf28388
Igor <18249562+djestat@users.noreply.github.com>
2022-03-12 17:58:48 +0300

    Lesson2
    - Layout

--- GB-SwiftUI/GB-SwiftUI/Core/Extensions/View.swift
+++ GB-SwiftUI/GB-SwiftUI/Core/Extensions/View.swift
@@ -6,3 +6,35 @@
 //
 
 import SwiftUI
+
+extension View {
+    ///Modifyed content
+    func debug() -> Self {
+        print(Mirror(reflecting: self).subjectType)
+        return self
+    }
+    
+    
+    /// View with cornerRadius, background, shadow, shadowRadius
+    func borderedBackground(cornerRadius: CGFloat,
+                            background: Color,
+                            shadow: Color,
+                            shadowRadius: CGFloat) -> some View {
+        self.modifier(
+            BorderShadowBackgroundViewModifier(cornerRadius: cornerRadius,
+                                               background: background,
+                                               shadow: shadow,
+                                               shadowRadius: shadowRadius)
+        )
+    }
+    
+    /// Avatar Image Modifier
+    func avatar(shadow: Color,
+                shadowRadius: CGFloat) -> some View {
+        self.modifier(
+            AvatarViewModifier(shadow: shadow,
+                               shadowRadius: shadowRadius)
+        )
+    }
+    
+}
